a322e046924002411e14400e1dc6adef3ac80057
Timmi-tech <tomiwatomzy66@gmail.com>
2025-06-03 02:05:29 +0100

    Add entrypoint.sh for Docker migration automation

--- ChatSystem-1.Api/entrypoint.sh
+++ ChatSystem-1.Api/entrypoint.sh
@@ -4,5 +4,5 @@ set -e
 # Run EF Core migrations
 dotnet ef database update --project ../ChatSystem-1.Infrastructure --startup-project . --no-build
 
-# Start the application
+# Start the application details
 exec dotnet ChatSystem-1.Api.dll
